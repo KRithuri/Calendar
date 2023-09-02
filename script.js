813660b340a28eb36c53e36ceed0ebe071dbbd62
Karabo Rithuri <rithurikarabo@gmail.com>
2023-09-02 20:30:54 +0200

    for loop to get today's date

--- script.js
+++ script.js
@@ -48,4 +48,14 @@ const renderCalendar = () => {
     for (let x = firstDayIndex; x > 0; x--) {
         days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
     }
+    for (let i = 1; i <= lastDay; i++) {
+        if (
+          i === new Date().getDate() &&
+          date.getMonth() === new Date().getMonth()
+        ) {
+          days += `<div class="today">${i}</div>`;
+        } else {
+          days += `<div>${i}</div>`;
+        }
+    }
 }
