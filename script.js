63a6972609132dae015778947c3f0ac64a9b2426
Karabo Rithuri <rithurikarabo@gmail.com>
2023-09-02 20:29:50 +0200

    initializing days

--- script.js
+++ script.js
@@ -42,4 +42,6 @@ const renderCalendar = () => {
 
     document.querySelector(".date h1").innerHTML = months[date.getMonth()];
     document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+    let days = "";
 }
