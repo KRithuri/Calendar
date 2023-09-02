bbfd1f6484058690a2c62eefc79f8b09de663ee0
Karabo Rithuri <rithurikarabo@gmail.com>
2023-09-02 20:27:38 +0200

    creating a list and fill it with months of the year

--- script.js
+++ script.js
@@ -24,4 +24,19 @@ const renderCalendar = () => {
     ).getDay();
 
     const nextDays = 7 - lastDayIndex - 1;
+
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
 }
